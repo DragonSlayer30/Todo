653ee13ddf2815fe2757a2cf82f6a00d45c6b766
Your Name <you@example.com>
2018-04-17 01:54:47 -0500

    Without Overdue

--- app/src/main/java/com/taskmanager/todo/DisplayMessageActivity.java
+++ app/src/main/java/com/taskmanager/todo/DisplayMessageActivity.java
@@ -1,10 +1,13 @@
 package com.taskmanager.todo;
 
 import android.content.Intent;
+import android.graphics.Paint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
 
 import model.Todo;
 
@@ -20,8 +23,13 @@ public class DisplayMessageActivity extends AppCompatActivity {
         Todo todo = MainActivity.all_tasks[index];
         TextView textView = findViewById(R.id.title);
         textView.setText(message);
+        if(todo.isDone()) {
+            textView.setTextColor(getColor(R.color.successFinished));
+        }
+        else  textView.setTextColor(getColor(R.color.unfinsihed));
+        SimpleDateFormat dt1 = new SimpleDateFormat("yyyy-MM-dd");
         TextView dueDate = findViewById(R.id.dueDate);
-        dueDate.setText(todo.getDue().toString());
+        dueDate.setText("Due : " + dt1.format(todo.getDue()));
         TextView description = findViewById(R.id.description);
         description.setText(todo.getDescription());
         //startActivity(intent);
--- app/src/main/java/com/taskmanager/todo/MainActivity.java
+++ app/src/main/java/com/taskmanager/todo/MainActivity.java
@@ -2,6 +2,7 @@ package com.taskmanager.todo;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Paint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -24,13 +25,13 @@ public class MainActivity extends AppCompatActivity {
 
     public static final String EXTRA_MESSAGE = "Main_Activity";
     public static String INDEX = "INDEX";
-    private static Todo test1 = new Todo( 0,"Test 1", "Sample Description 1 Sample Description 1 Sample Description 1 Sample Description 1 Sample Description 1", false, new Date());
+    private static Todo test1 = new Todo( 0,"Test 1", "Sample Description 1", false, new Date());
     private static Todo test2 = new Todo(1, "Test 2", "Sample Description 2", true, new Date());
-    private static Todo test3 = new Todo(1, "Test 3", "Sample Description 2", true, new Date());
+    private static Todo test3 = new Todo(1, "Test 3", "Sample Description 2", false, new Date());
     private static Todo test4 = new Todo(1, "Test 4", "Sample Description 2", true, new Date());
-    private static Todo test5 = new Todo(1, "Test 5", "Sample Description 2", true, new Date());
+    private static Todo test5 = new Todo(1, "Test 5", "Sample Description 2", false, new Date());
     private static Todo test6 = new Todo(1, "Test 6", "Sample Description 2", true, new Date());
-    private static Todo test7 = new Todo(1, "Test 7", "Sample Description 2", true, new Date());
+    private static Todo test7 = new Todo(1, "Test 7", "Sample Description 2", false, new Date());
     private static Todo test8 = new Todo(1, "Test 8", "Sample Description 2", true, new Date());
     //public static Todo[] all_tasks = {test1, test2, test3, test4, test5, test6, test7, test8, test1, test2, test3, test4, test5, test6, test7, test8};
     public static Todo[] all_tasks = {test1, test2, test3, test4, test5, test6, test7, test8};
@@ -62,19 +63,26 @@ public class MainActivity extends AppCompatActivity {
     private void add_tasks_to_view(ViewGroup view, Todo[] tasks) {
         LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
         params.width = LinearLayout.LayoutParams.MATCH_PARENT;
+        params.setMargins(15,15,15,15);
         params.weight = 1;
-        params.setMargins(10,5,10,5);
         Context context = this.getApplicationContext();
         int ind = 0;
         int height = 0;
         for (Todo task: tasks) {
             final TextView task_title = new TextView(context);
             task_title.setText(task.getTitle());
-            task_title.setTextSize(30);
+            task_title.setTextSize(25);
             task_title.setLayoutParams(params);
             task_title.setBackground(getDrawable(R.drawable.border));
-            task_title.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
-            task_title.setGravity(Gravity.CENTER);
+            task_title.setBackgroundColor(getColor(R.color.whiteBackground));
+            task_title.setTextAlignment(View.TEXT_ALIGNMENT_VIEW_START);
+            task_title.setPadding(15,0,0,0);
+            if(task.isDone()) {
+                task_title.setTextColor(getColor(R.color.successFinished));
+                task_title.setPaintFlags(task_title.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
+            }
+            else  task_title.setTextColor(getColor(R.color.unfinsihed));
+            //task_title.setGravity(Gravity.CENTER);
             final int index = ind;
             task_title.setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -86,45 +94,6 @@ public class MainActivity extends AppCompatActivity {
             view.addView(task_title);
             ind = ind + 1;
         }
-        ind = 0;
-        for (Todo task: tasks) {
-            final TextView task_title = new TextView(context);
-            task_title.setText(task.getTitle());
-            task_title.setTextSize(30);
-            task_title.setLayoutParams(params);
-            task_title.setBackground(getDrawable(R.drawable.border));
-            task_title.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
-            task_title.setGravity(Gravity.CENTER);
-            final int index = ind;
-            task_title.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    sendMessage(view, index);
-                }
-            });
-            height = task_title.getHeight();
-            view.addView(task_title);
-            ind = ind + 1;
-        }
-        ind = 0;
-        for (Todo task: tasks) {
-            final TextView task_title = new TextView(context);
-            task_title.setText(task.getTitle());
-            task_title.setTextSize(30);
-            task_title.setLayoutParams(params);
-            task_title.setBackground(getDrawable(R.drawable.border));
-            task_title.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
-            task_title.setGravity(Gravity.CENTER);
-            final int index = ind;
-            task_title.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    sendMessage(view, index);
-                }
-            });
-            height = task_title.getHeight();
-            view.addView(task_title);
-            ind = ind + 1;
-        }
+
     }
 }
