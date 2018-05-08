0a071fd42b9f326fb7ae30531d8a677dc6d6797c
Your Name <you@example.com>
2018-05-08 02:41:47 -0500

    With Non working Menu

--- app/src/main/java/com/taskmanager/todo/DisplayMessageActivity.java
+++ app/src/main/java/com/taskmanager/todo/DisplayMessageActivity.java
@@ -1,9 +1,16 @@
 package com.taskmanager.todo;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Paint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -21,17 +28,23 @@ public class DisplayMessageActivity extends AppCompatActivity {
         String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
         int index = Integer.parseInt(intent.getStringExtra(MainActivity.INDEX));
         Todo todo = MainActivity.all_tasks[index];
-        TextView textView = findViewById(R.id.title);
-        textView.setText(message);
-        if(todo.isDone()) {
-            textView.setTextColor(getColor(R.color.successFinished));
-        }
-        else  textView.setTextColor(getColor(R.color.unfinsihed));
+        final TextView editText = findViewById(R.id.title);
+        editText.setText(message);
         SimpleDateFormat dt1 = new SimpleDateFormat("yyyy-MM-dd");
         TextView dueDate = findViewById(R.id.dueDate);
         dueDate.setText("Due : " + dt1.format(todo.getDue()));
         TextView description = findViewById(R.id.description);
         description.setText(todo.getDescription());
-        //startActivity(intent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.task_activity, menu);
+        return true;
+    }
+
+    public void changeTtile() {
+
     }
 }
